40ff071ba8271ede92bc6d0b993b366714de36b0
tengju <tengjuchi@gmail.com>
2021-02-01 12:00:43 +0100

    update please work

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,10 @@
 import * as a1lib from "@alt1/base";
 import Ability from "@alt1/ability";
 
-const appConfig = require("!file-loader?name=[name].[ext]!./appconfig.json");
+require("!file-loader?name=[name].[ext]!./appconfig.json");
 require("!file-loader?name=[name].[ext]!./index.html");
 require("!file-loader?name=[name].[ext]!./Prayer.png");
 
-
-
-//You can reach exports on window.TEST because of
-//config.makeUmd("testpackage", "TEST"); in webpack.config.ts
 var img = a1lib.captureHoldFullRs();
 const ability = new Ability([]);
 export function capture() {
@@ -18,8 +14,5 @@ export function capture() {
 }
 
 if (window.alt1) {
-	//tell alt1 about the app
-	//this makes alt1 show the add app button when running insane the embedded browser
-	//also updates app settings if they are changed
-	alt1.identifyAppUrl(appConfig.configUrl);
+	alt1.identifyAppUrl("https://tengju.github.io/better-bars/release/appconfig.json");
 }
